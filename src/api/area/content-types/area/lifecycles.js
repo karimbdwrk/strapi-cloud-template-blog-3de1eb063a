958c3adf274015f2bcbc4f6161f38674cae90c1b
Karim Badwork <karim@badwork.fr>
2025-04-14 02:22:47 +0200

    feat: Ajouter le cycle de vie afterCreate pour l'envoi d'un email lors de la création d'une nouvelle zone

---
+++ src/api/area/content-types/area/lifecycles.js
@@ -0,0 +1,16 @@
+module.exports = {
+  async afterCreate(event) {
+    const { result } = event;
+
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: "krimobadworker@gmail.com",
+        from: "karim@badwork.fr",
+        subject: "You have a new area",
+        text: `New area UUID : ${result.name}`,
+      });
+    } catch (error) {
+      console.log("error email send :", error);
+    }
+  },
+};
